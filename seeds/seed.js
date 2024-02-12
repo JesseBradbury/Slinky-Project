61d97d5c6f6d4974fbf96f20679cea36bb55c341
JB <bradburyjesse7@gmail.com>
2024-02-12 16:37:15 -0700

    Working on routes for heroku

--- seeds/seed.js
+++ seeds/seed.js
@@ -5,8 +5,8 @@ const sequelize = require('../config/connection');
 const { User, Spot, Wave } = require('../models');
 
 const userData = require('./userData.json');
-const spotData = require('./spotData.json');
-const waveData = require('./waveData.json');
+// const spotData = require('./spotData.json');
+// const waveData = require('./waveData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -15,14 +15,14 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
-    await Spot.bulkCreate(spotData, {
-        individualHooks: true,
-        returning: true,
-    });
-    await Wave.bulkCreate(waveData, {
-        individualHooks: true,
-        returning: true,
-    });
+    // await Spot.bulkCreate(spotData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // });
+    // await Wave.bulkCreate(waveData, {
+    //     individualHooks: true,
+    //     returning: true,
+    // });
     process.exit(0);
 };
 
